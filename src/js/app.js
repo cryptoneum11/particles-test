66145d4238fd89626e2e7ba3898f576f8b95369d
Eric Posas <eric.posas@sector5digital.com>
2019-01-21 18:33:30 -0600

    10,000 particles added

--- src/js/app.js
+++ src/js/app.js
@@ -6,8 +6,10 @@ import PixiFps from 'pixi-fps';
 const fpsCounter = new PixiFps();
 // modules
 import utils from './modules/myutils.js';
+import hittest from './modules/hittest.js';
 // images
 import '../images/circle.png';
+import '../images/purp.png';
 
 
 let type = "WebGL"
@@ -27,19 +29,32 @@ app.renderer.view.style.position = "absolute";
 app.renderer.view.style.display = "block";
 app.renderer.autoResize = true;
 
+let purp, sprites = [], num_of_particles = 20000;
+
+
 $( 'body' ).append( app.view );
 // app.stage.addChild(fpsCounter);
 
 PIXI.loader
   .add( '../images/circle.png' )
+  .add( '../images/purp.png' )
   .load( setup );
 
+
 function setup(){
-  for( var i = 0; i<5000; i++ ){
+
+  purp = new PIXI.Sprite( PIXI.loader.resources[ '../images/purp.png' ].texture );
+  purp.anchor.set( .5, .5 );
+  purp.position.set( innerWidth/2, innerHeight/2 );
+  app.stage.addChild( purp );
+  purp.visible = false;
+
+  for( var i = 0; i < num_of_particles; i++ ){
     let sprite = new PIXI.Sprite( PIXI.loader.resources[ '../images/circle.png' ].texture );
-    sprite.position.x = utils.getRandomInt( 0, innerWidth );
-    sprite.position.y = utils.getRandomInt( 0, innerHeight );
+    sprite.position.set( utils.getRandomInt(0,innerWidth), utils.getRandomInt(0,innerHeight) );
+    sprite.width = sprite.height = utils.getRandomInt( 0, 10 );
     app.stage.addChild( sprite );
+    sprites.push( sprite );
   }
 
   // add_slider();
@@ -58,5 +73,10 @@ function setup(){
 
 
 $( window ).resize( function(){
+  purp.position.set( innerWidth/2, innerHeight/2 );
+  for(var i = 0; i<sprites.length; i++){
+    if(sprites[i])
+      sprites[i].position.set( utils.getRandomInt(0,innerWidth), utils.getRandomInt(0,innerHeight) );
+  }
   app.renderer.resize(innerWidth, innerHeight);
 });
